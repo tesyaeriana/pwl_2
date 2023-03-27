305dfb8e69edaa48490011073c5d2182fa4680be
tesyaeriana <2141720024@student.polinema.ac.id>
2023-03-27 11:15:50 +0700

    pertemuan7_autentikasi

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request as HttpRequest;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/dashboard';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function showLoginForm()
+    {
+        return view('layouts.login');
+    }
+    public function username()
+    {
+        return 'username';
+    }
+    protected function validateLogin(HttpRequest $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string|min:6',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ControllerContactUs;
 use App\Http\Controllers\ControllerDashboard;
 use App\Http\Controllers\ControllerNews;
@@ -17,6 +18,7 @@ use App\Http\Controllers\MataKuliahController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,83 +36,91 @@ use Illuminate\Support\Facades\Route;
 //return view('welcome');
 //});
 //Praktikum 1
-Route::get('/', function (){
-    echo "Selamat Datang";
-});
-Route::get('/about', function (){
-    echo "NIM:2141720024 Nama:Tesya Eriana";
-});
-Route::get('/articles/{id}', function ($id){
-  echo "Halaman Artikel dengan ID";
-  return $id;
-});
 
-//Praktikum 2
 
-route::get('/',[PageController::class, 'index']);
 
-route::get('/about',[PageController::class, 'about']);
 
-route::get('/articles/{id}',[PageController::class, 'articles']);
 
-//No3
-route::get('/',[HomeController::class, 'index']);
-
-route::get('/',[AboutController::class, 'index']);
-
-route::get('/',[ArticleController::class, 'index']);
-
-//Praktikum 3
-route::get('/home/{nama}',function($nama){
-    echo "Selamat datang ";
-    return $nama;
-});
-
-Route::prefix('produk')->group(function () {
-    Route::get('/list', [ControllerProduk::class, 'produk']);
-});
-
-route::get('user/{id}',function($id){
-   
-    return 'user'.$id;
-});
-
-Route::prefix('program')->group(function () {
-    Route::get('/list', [PageController::class, 'program']);
-});
-
-Route::get('/about', function (){
-    echo "<a href='https://www.educastudio.com/about-us '>Tentang Kami</a>";
-});
-//Route::resource('contactUs', PageController::class);
-
-Route::get('/hello',[WelcomeController::class, 'hello']
-    //return View::make('hello', ['nama'=>'Tesya']);
-    //return view('blog.hello', ['nama' => 'Asisah']);
+Auth::Routes();
+Route::get('/logout',[LoginController::class, 'logout']);
+Route::middleware(['auth']) ->group(function(){
+    Route::get('/', function (){
+        echo "Selamat Datang";
+    });
+    Route::get('/about', function (){
+        echo "NIM:2141720024 Nama:Tesya Eriana";
+    });
+    Route::get('/articles/{id}', function ($id){
+      echo "Halaman Artikel dengan ID";
+      return $id;
+    });
     
-);
-Route::resource('index', PageController::class);
-
-Route::get('/news',[ControllerNews::class, 'news']);
-
-Route::get('/program',[ControllerProgram::class, 'program']);
-
-Route::get('/aboutus',[AboutController::class,'aboutus']);
-
-//Route::resource('index',[ControllerContactUs::class]);
-Route::get('/',function(){
-    return view('layouts.tamplate');
+    //Praktikum 2
+    
+    route::get('/',[PageController::class, 'index']);
+    
+    route::get('/about',[PageController::class, 'about']);
+    
+    route::get('/articles/{id}',[PageController::class, 'articles']);
+    
+    //No3
+    route::get('/',[HomeController::class, 'index']);
+    
+    route::get('/',[AboutController::class, 'index']);
+    
+    route::get('/',[ArticleController::class, 'index']);
+    
+    //Praktikum 3
+    route::get('/home/{nama}',function($nama){
+        echo "Selamat datang ";
+        return $nama;
+    });
+    
+    Route::prefix('produk')->group(function () {
+        Route::get('/list', [ControllerProduk::class, 'produk']);
+    });
+    
+    route::get('user/{id}',function($id){
+       
+        return 'user'.$id;
+    });
+    
+    Route::prefix('program')->group(function () {
+        Route::get('/list', [PageController::class, 'program']);
+    });
+    
+    Route::get('/about', function (){
+        echo "<a href='https://www.educastudio.com/about-us '>Tentang Kami</a>";
+    });
+    //Route::resource('contactUs', PageController::class);
+    
+    Route::get('/hello',[WelcomeController::class, 'hello']
+        //return View::make('hello', ['nama'=>'Tesya']);
+        //return view('blog.hello', ['nama' => 'Asisah']);
+        
+    );
+    Route::resource('index', PageController::class);
+    
+    Route::get('/news',[ControllerNews::class, 'news']);
+    
+    Route::get('/program',[ControllerProgram::class, 'program']);
+    
+    Route::get('/aboutus',[AboutController::class,'aboutus']);
+    
+    //Route::resource('index',[ControllerContactUs::class]);
+    Route::get('/',function(){
+        return view('layouts.tamplate');
+    });
+    //pertemuan 7
+    Route::get('/dashboard',[ControllerDashboard::class, 'index']);
+    Route::get('/profile',[ProfileController::class, 'index']);
+    Route::get('/kuliah',[KuliahController::class, 'index']);
+    Route::get('/kendaraan',[KendaraanController::class, 'index']);
+    Route::get('/hobi',[HobiController::class,'index']);
+    Route::get('/keluarga',[KeluargaController::class,'index']);
+    Route::get('/matakuliah',[MataKuliahController::class,'index']);
 });
-Route::get('/dashboard',[ControllerDashboard::class, 'index']);
-Route::get('/profile',[ProfileController::class, 'index']);
-Route::get('/kuliah',[KuliahController::class, 'index']);
 
-Route::get('/kendaraan',[KendaraanController::class, 'index']);
 
-Route::get('/hobi',[HobiController::class,'index']);
-
-Route::get('keluarga',[KeluargaController::class,'index']);
-
-Route::get('/matakuliah',[MataKuliahController::class,'index']);
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
