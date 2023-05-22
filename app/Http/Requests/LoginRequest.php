c86d66fd35e5329c3557471330d6cfbc91821af9
tesyaeriana <2141720024@student.polinema.ac.id>
2023-05-22 11:13:20 +0700

    praktikum1

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email'=>'required|sttring|email',
+            'password'=>'required|string|min:8',
+        ];
+    }
+    
+}
